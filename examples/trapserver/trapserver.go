36e6a70f80336cb4b2e6885e475b249806f5f165
Ross Peoples <ross.peoples@gmail.com>
2016-08-10 21:17:10 -0500

    Better log formatting and note on usage in Unix environments.

--- examples/trapserver/trapserver.go
+++ examples/trapserver/trapserver.go
@@ -1,6 +1,12 @@
+// Package main provides an example of how to create an SNMP trap receiver server.
+// NOTE: In Unix environments (Mac, Linux, etc) this needs to be run as root or
+// using "sudo" since this requires port 162 to run and ports in those environments
+// below 1024 are protected. You may get a "permission denied" error trying to run
+// this without root or "sudo".
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net"
 
@@ -14,7 +20,8 @@ func (h snmpHandler) OnError(addr net.Addr, err error) {
 }
 
 func (h snmpHandler) OnTrap(addr net.Addr, trap snmplib.Trap) {
-	log.Println(addr.String(), trap)
+	prettyPrint, _ := json.MarshalIndent(trap, "", "\t")
+	log.Println(string(prettyPrint))
 }
 
 func main() {
